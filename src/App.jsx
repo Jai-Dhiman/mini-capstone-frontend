d409f840f010018122f2796575aeaaf9d5bdd9be
JaiDhiman <jaidhiman2000@gmail.com>
2024-09-25 16:45:24 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { ProductPage } from "./ProductPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <ProductPage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
